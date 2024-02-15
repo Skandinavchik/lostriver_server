361accbec96b9a36880240c4636ad2b17d7e4031
Artem Sokolov <artemsokolov2569@gmail.com>
2024-02-15 19:47:17 +0100

    feat: refactor WaterModule and WaterService for better code organization and readability

--- src/water/water.service.ts
+++ src/water/water.service.ts
@@ -1,161 +1,162 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common'
-import { CreateWaterDto } from './dto/create-water.dto'
-import { UpdateWaterDto } from './dto/update-water.dto'
-import { PrismaService } from '../prisma/prisma.service'
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
-import { WatersResponse, SingleWaterResponse } from './entities/water-response.entity'
-import { LicenseType, WaterType } from '@prisma/client'
-import { GetWatersArgs } from './dto/get-waters.dto'
-
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { LicenseType, WaterType } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateWaterDto } from './dto/create-water.dto';
+import { GetWatersArgs } from './dto/get-waters.dto';
+import { UpdateWaterDto } from './dto/update-water.dto';
+import {
+  SingleWaterResponse,
+  WatersResponse,
+} from './entities/water-response.entity';
 
 @Injectable()
 export class WaterService {
-	constructor(private readonly prismaService: PrismaService) { }
+  constructor(private readonly prismaService: PrismaService) {}
 
-	async create(createWaterDto: CreateWaterDto): Promise<SingleWaterResponse> {
-		try {
-			const water = await this.prismaService.water.create({
-				data: createWaterDto,
-				include: {
-					organization: true
-				}
-			})
+  async create(createWaterDto: CreateWaterDto): Promise<SingleWaterResponse> {
+    try {
+      const water = await this.prismaService.water.create({
+        data: createWaterDto,
+        include: {
+          organization: true,
+        },
+      });
 
-			return {
-				water
-			}
+      return {
+        water,
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        throw new ConflictException('Water already exists');
+      }
+    }
+  }
 
-		} catch (error) {
-			if (error instanceof PrismaClientKnownRequestError) {
-				throw new ConflictException('Water already exists')
-			}
-		}
-	}
+  async getMany(args: GetWatersArgs): Promise<WatersResponse> {
+    try {
+      const { q, order, wtypes, ltypes, take, skip } = args;
+      const totalResults = await this.prismaService.water.count();
+      const waters = await this.prismaService.water.findMany({
+        where: {
+          OR: [
+            {
+              title: {
+                contains: q,
+                mode: 'insensitive',
+              },
+            },
+            {
+              serialNumber: {
+                contains: q,
+              },
+            },
+            {
+              organization: {
+                title: {
+                  contains: q,
+                  mode: 'insensitive',
+                },
+              },
+            },
+          ],
+          waterType: {
+            in: wtypes && wtypes.length ? wtypes : Object.values(WaterType),
+          },
+          licenseType: {
+            hasSome:
+              ltypes && ltypes.length ? ltypes : Object.values(LicenseType),
+          },
+        },
+        include: {
+          organization: true,
+        },
+        orderBy: order,
+        take,
+        skip,
+      });
 
-	async getMany(args: GetWatersArgs): Promise<WatersResponse> {
-		try {
-			const { q, order, wtypes, ltypes, take, skip } = args
-			const totalResults = await this.prismaService.water.count()
-			const waters = await this.prismaService.water.findMany({
-				where: {
-					OR: [
-						{
-							title: {
-								contains: q,
-								mode: 'insensitive',
-							},
-						},
-						{
-							serialNumber: {
-								contains: q,
-							},
-						},
-						{
-							organization: {
-								title: {
-									contains: q,
-									mode: 'insensitive'
-								}
-							}
-						},
-					],
-					waterType: {
-						in: wtypes && wtypes.length
-							? wtypes
-							: Object.values(WaterType)
-					},
-					licenseType: {
-						hasSome: ltypes && ltypes.length
-							? ltypes
-							: Object.values(LicenseType)
-					}
-				},
-				include: {
-					organization: true,
-				},
-				orderBy: order,
-				take,
-				skip
-			})
+      return {
+        totalResults,
+        currentResults: waters.length,
+        waters,
+      };
+    } catch (error) {
+      console.log(error);
+      if (error instanceof Error) {
+        throw new Error();
+      }
+    }
+  }
 
-			return {
-				totalResults,
-				currentResults: waters.length,
-				waters
-			}
+  async getById(serialNumber: string): Promise<SingleWaterResponse> {
+    try {
+      const water = await this.prismaService.water.findUniqueOrThrow({
+        where: {
+          serialNumber,
+        },
+        include: {
+          organization: true,
+        },
+      });
 
-		} catch (error) {
-			console.log(error)
-			if (error instanceof Error) {
-				throw new Error()
-			}
-		}
-	}
+      return {
+        water,
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        throw new NotFoundException('Water not found');
+      }
+    }
+  }
 
-	async getById(id: string): Promise<SingleWaterResponse> {
-		try {
-			const water = await this.prismaService.water.findUniqueOrThrow({
-				where: {
-					id,
-				},
-				include: {
-					organization: true
-				}
-			})
+  async update(
+    id: string,
+    updateWaterDto: UpdateWaterDto,
+  ): Promise<SingleWaterResponse> {
+    try {
+      const updatedWater = await this.prismaService.water.update({
+        where: {
+          id,
+        },
+        data: updateWaterDto,
+        include: {
+          organization: true,
+        },
+      });
 
-			return {
-				water
-			}
+      return {
+        water: updatedWater,
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        throw new NotFoundException('Water not found');
+      }
+    }
+  }
 
-		} catch (error) {
-			if (error instanceof PrismaClientKnownRequestError) {
-				throw new NotFoundException('Water not found')
-			}
-		}
-	}
+  async remove(id: string): Promise<SingleWaterResponse> {
+    try {
+      const removedWater = await this.prismaService.water.delete({
+        where: {
+          id,
+        },
+        include: {
+          organization: true,
+        },
+      });
 
-	async update(id: string, updateWaterDto: UpdateWaterDto): Promise<SingleWaterResponse> {
-		try {
-			const updatedWater = await this.prismaService.water.update({
-				where: {
-					id
-				},
-				data: updateWaterDto,
-				include: {
-					organization: true,
-				}
-			})
-
-			return {
-				water: updatedWater
-			}
-
-		} catch (error) {
-			if (error instanceof PrismaClientKnownRequestError) {
-				throw new NotFoundException('Water not found')
-			}
-		}
-	}
-
-	async remove(id: string): Promise<SingleWaterResponse> {
-		try {
-			const removedWater = await this.prismaService.water.delete({
-				where: {
-					id
-				},
-				include: {
-					organization: true
-				}
-			})
-
-			return {
-				water: removedWater
-			}
-
-		} catch (error) {
-			if (error instanceof PrismaClientKnownRequestError) {
-				throw new NotFoundException('Water not found')
-			}
-		}
-	}
+      return {
+        water: removedWater,
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        throw new NotFoundException('Water not found');
+      }
+    }
+  }
 }
